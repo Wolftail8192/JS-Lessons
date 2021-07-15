cd111eea5278c97a0391dffd154af2fe55e58392
Wolftail8192 <4420299@gmail.com>
2021-07-15 17:53:46 +0300

    hw2+

---
+++ JS 2 HW/main.js
@@ -0,0 +1,13 @@
+document.write(`<div>`);
+
+for (const user of users) {
+    document.write(`<div>
+                    ${userid}, ${id}, ${title}, ${body}   
+                    </div>`);
+}
+
+
+document.write(`</div>`);
+
+document.write(`<div>`);
+document.write(`</div>`);
